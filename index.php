0b43a025ae690f9368ff00cdc4ce0d05f4e52478
Cristian Gonzalez <cris.andres.gz@gmail.com>
2023-10-11 21:51:33 -0300

    incorpore la lista de categorias

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-function getRecetario() {
+function getPlatos() {
     $db = new PDO('mysql:host=localhost;dbname=recetario;charset=utf8', 'root', '');
     $query = $db->prepare('SELECT * FROM platos');
     $query->execute();
@@ -8,10 +8,33 @@ function getRecetario() {
     return $platos;
 }
 
-$platos = getRecetario();
+$platos = getPlatos();
 echo "<ul>";
 foreach ($platos as $plato) {
     echo "<li>$plato->nombre_plato <a href='detalle_platos.php?id=$plato->plato_id'>Ver detalles</a></li>";
 }
 echo "</ul>";
+
+
+
+function getCategoria() {
+    $db = new PDO('mysql:host=localhost;dbname=recetario;charset=utf8', 'root', '');
+    $query = $db->prepare('SELECT * FROM categoria');
+    $query->execute();
+    $categorias = $query->fetchAll(PDO::FETCH_OBJ);
+
+    return $categorias;
+}
+
+
+$categorias = getCategoria();
+
+echo "<ul>";
+foreach ($categorias as $categoria) {
+    echo "<li>$categoria->categoria <a href='detalle_categoria.php?id=$categoria->categoria_id'>Ver mas</a></li>";
+}
+echo "</ul>";
 ?> 
+
+
+
